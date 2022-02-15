79d43ae135d1c2bab7c8fd93aed60f3563e9be53
Mohamed Elgendy <54114536+M-R-Elgendy@users.noreply.github.com>
2022-02-16 00:43:21 +0200

    Install express And run the server

---
+++ get and post/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("First msg")
+})
+
+
+
+const port = 3000;
+app.listen(port, listening)
+
+function listening() {
+    console.log(`Server ruinng on http://localhost:${port}`)
+}
